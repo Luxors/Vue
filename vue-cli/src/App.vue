0793285b860cbdd9c42824e538b31c84a7f8d3ef
Luxors <luxors@mail.com>
2019-04-20 11:44:16 +0300

    vue-cli init

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="wrapper">
+	<div class="wrapper-content">
+		<section>
+			<div class="containet">
+				<div id="app">
+					<img alt="Vue logo" src="./assets/logo.png">
+				</div>
+			</div>
+		</section>
+	</div>
+</div>
+  
+</template>
+
+<script>
+export default {}
+</script>
