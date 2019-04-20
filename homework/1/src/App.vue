f17464d6d0aff2ce9b3be2195a6a3dfbbdf398fe
Luxors <luxors@mail.com>
2019-04-20 12:51:35 +0300

    homework

---
+++ homework/1/src/App.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+
+      <section>
+        <div class="container">
+          <div class="note-header" style="margin: 36px 0; justify-content: center;">
+            <p> {{ title }} </p>
+          </div>
+          <!-- message -->
+          <Message v-if="message" :message="message"/>
+
+          <!-- new note -->
+          <NewNote :note="note" @addNote="addNote"/>
+
+          <div class="note-header" style="margin: 36px 0;">
+            <!-- title -->
+            <h1> {{ title }} </h1>
+
+            <!-- search -->
+            <Search
+              :value="search"
+              placeholder="Find your note"
+              @search="search = $event"/>
+
+            <!-- icons controls -->
+            <div class="icons">
+              <button type="button" @click="grid = true">
+								<svg :class="{ 'active' : grid }" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
+							</button>
+							<button type="button" @click="grid = false">
+								<svg :class="{ 'active' : !grid }" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
+							</button>
+            </div>
+          </div>
+
+          <!-- note list -->
+          <Notes :notes="notesFilter" :grid="grid" @remove="removeNote" />
+
+        </div>
+      </section>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Search from '@/components/Search'
+import Message from '@/components/Message'
+import NewNote from '@/components/NewNote'
+import Notes from '@/components/Notes'
+
+export default {
+	components: {
+		Search,
+		Message,
+		NewNote,
+		Notes
+	},
+  data () {
+    return {
+      title: 'Notes App',
+      search: '',
+      message: null,
+      grid: true,
+      note: {
+				title: '',
+				priority: 'normal',
+        descr: ''
+      },
+      notes: [
+        {
+					title: 'First Note',
+					priority: 'normal',
+          descr: 'Description for first note',
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+					title: 'Second Note',
+					priority: 'medium',
+          descr: 'Description for second note',
+          date: new Date(Date.now()).toLocaleString()
+        },
+        {
+					title: 'Third Note',
+					priority: 'hight',
+          descr: 'Description for third note',
+          date: new Date(Date.now()).toLocaleString()
+        }
+      ]
+    }
+  },
+  computed: {
+    notesFilter () {
+      let array = this.notes,
+          search = this.search
+      if (!search) return array
+      // Small
+      search = search.trim().toLowerCase()
+      // Filter
+      array = array.filter(function (item) {
+        if (item.title.toLowerCase().indexOf(search) !== -1) {
+          return item
+        }
+      })
+      // Error
+      return array
+    }
+  },
+  methods: {
+    addNote () {
+      // console.log(this.note)
+      let {title, priority, descr} = this.note
+
+      if (title === '') {
+        this.message = 'title can`t be blank!'
+        return false
+      }
+
+      this.notes.push({
+				title,
+				priority,
+        descr,
+        date: new Date(Date.now()).toLocaleString()
+      })
+      this.message = null
+			this.note.title = ''
+			this.note.priority = 'normal'
+      this.note.descr = ''
+    },
+    removeNote (index) {
+      this.notes.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 800px;
+}
+.icons {
+	button {
+		background-color: transparent;
+		border: 0;
+		cursor: pointer;
+	}
+}
+</style>
---
+++ homework/1/src/components/NewNote.vue
@@ -0,0 +1,66 @@
+<template>
+  <!-- new note -->
+  <div class="new-note">
+
+		<p class="input-wrap">
+			<label for="title">Title</label>
+			<input v-model="note.title" type="text" id="title">
+		</p>
+
+		<p class="input-wrap">
+			<label for="description">Description</label>
+			<textarea v-model="note.descr" id="description"></textarea>
+		</p>
+
+		<fieldset class="new-note__priority">
+			<legend >Priority</legend>
+
+			<Radio v-model="note.priority" val="normal" caption="Normal" />
+
+			<Radio v-model="note.priority" val="medium" caption="Medium" />
+
+			<Radio v-model="note.priority" val="hight" caption="Hight" />
+			
+		</fieldset>
+
+    <button class="btn btnPrimary" @click="addNote">New note</button>
+
+		<!-- <div>Priority: {{ note.priority }}</div> -->
+  </div>
+</template>
+
+<script>
+import Radio from '@/components/Radio'
+
+export default {
+	components: {
+		Radio
+	},
+  props: {
+    note: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    addNote () {
+      this.$emit('addNote', this.note)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.new-note {
+  text-align: center;
+}
+.new-note__priority {
+	margin-bottom: 50px;
+	border: 0;
+
+	legend {
+		margin-bottom: 10px;
+	}
+}
+</style>
+
