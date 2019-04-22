99e353fa48309df77b280e055070073785153066
Luxors <luxors@mail.com>
2019-04-22 19:50:37 +0300

    +

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,7 +2,7 @@
 <div class="wrapper">
 	<div class="wrapper-content">
 		<section>
-			<div class="containet">
+			<div class="container">
 				<div id="app">
 					<img alt="Vue logo" src="./assets/logo.png">
 				</div>
