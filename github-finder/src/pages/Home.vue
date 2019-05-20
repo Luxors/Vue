f217dc0a77c98eea28981fdd5caf388d3d16aa79
Luxors <luxors@mail.com>
2019-05-20 20:20:55 +0300

    #2 Search

--- github-finder/src/pages/Home.vue
+++ github-finder/src/pages/Home.vue
@@ -3,9 +3,42 @@
 	<div class="wrapper-content wrapper-content--fixed">
 		<section>
 			<div class="container">
-				<h1>Home page</h1>
+				<Search :value="search" placeholder="Type username..." @search="search = $event" />
+				<!-- <p>{{search}}</p> -->
+				<button class="btn btnPrimary" @click="getRepos">Search</button>
 			</div>
 		</section>
 	</div>
 	
 </template>
+
+<script>
+import Search from '@/components/Search'
+export default {
+	components: {
+		Search
+	},
+	data() {
+		return {
+			search: ''
+		}
+	},
+	methods: {
+		getRepos() {
+			console.log(`get user ${this.search} repos`)
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+button {
+	margin-top: 40px;
+}
+</style>
+
