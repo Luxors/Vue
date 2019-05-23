0750546a82ba4919b8cfb0b413e0feca802b37dc
Luxors <luxors@mail.com>
2019-05-23 20:39:23 +0300

    completed

--- homework/github-finder/light/src/pages/Home.vue
+++ homework/github-finder/light/src/pages/Home.vue
@@ -10,11 +10,25 @@
 
 				<Search :value="search" placeholder="Type username..." @search="search = $event" />
 				<!-- <p>{{search}}</p> -->
-				<button class="btn btnPrimary" @click="getRepos">
+				<button class="btn btnPrimary" @click="getUserData">
 					<span v-if="!repos">Search</span>
 					<span v-else>Search Again</span>
 				</button>
 
+				<!-- <div>
+					<img v-if="avatar" :src="avatar" alt="It's me">
+					<div>
+						<p v-if="name"> {{ name }} </p>
+						<p v-if="reposQuantity" > Repositories: {{ reposQuantity }} </p>
+					</div>
+				</div> -->
+				<div v-if="info" class="info">
+					<img class="info-ava" :src="info.avatar_url" alt="It's me">
+					<div>
+						<p class="info-name"> {{ info.name }} </p>
+						<p> Repositories: {{ info.public_repos }} </p>
+					</div>
+				</div>
 				<div class="repos__wrapper" v-if="repos">
 					<div class="repo-item" v-for="repo in repos" :key="repo.id">
 						<div class="repo-info">
@@ -42,24 +56,36 @@ export default {
 		return {
 			search: '',
 			error: null,
+			// avatar: null,
+			// name: null,
+			// reposQuantity: null,
+			info: null,
 			repos: null
 		}
 	},
 	methods: {
-		getRepos() {
-			// console.log(`get user ${this.search} repos`)
-			axios
-				.get(`https://api.github.com/users/${this.search}/repos`)
-					.then(res => {
-						console.log(res)
-						this.error = null
-						this.repos = res.data
-					})
-					.catch(err => {
-						console.log(err)
-						this.repos = null
-						this.error = 'Can`t Take My Hands Off You'
-					})
+		getUserInfo() {
+			return axios.get(`https://api.github.com/users/${this.search}`)
+		},
+		getUserRepos() {
+			return axios.get(`https://api.github.com/users/${this.search}/repos`)
+		},
+		getUserData() {
+			axios.all([this.getUserInfo(), this.getUserRepos()])
+				.then(axios.spread((info, repos) => {
+					// console.log(info)
+					this.error = null
+					// this.avatar = info.data.avatar_url
+					// this.name = info.data.name
+					// this.reposQuantity = info.data.public_repos
+					this.info = info.data
+					this.repos = repos.data
+				}))
+				.catch(err => {
+					// console.log(err)
+					this.repos = null
+					this.error = 'Can`t Take My Hands Off You'
+				})
 		}
 	}
 }
@@ -72,7 +98,7 @@ export default {
 	align-items: center;
 }
 button {
-	margin-top: 40px;
+	margin: 40px 0;
 }
 .repos__wrapper {
 	max-width: 450px;
@@ -87,6 +113,20 @@ button {
 	border-bottom: 1px solid #dbdbdb;
 }
 .error {
+	margin-bottom: 10px;
+}
+.info {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+.info-ava {
+	width: 100px;
+	height: 100px;
+	margin-right: 20px;
+	border-radius: 100%;
+}
+.info-name {
 	margin-bottom: 10px;
 }
 </style>
