b8aaa57d472da76982efd2e65abe41d1ec823a5c
Luxors <luxors@mail.com>
2019-08-06 22:33:16 +0300

    #17,18

---
+++ nuxt-blog/components/admin/NewPostForm.vue
@@ -0,0 +1,47 @@
+<template>
+	<section class="new-post">
+		<div class="container">
+			<form @submit.prevent>
+				<AppInput v-model="post.title">Title:</AppInput>
+				<AppInput v-model="post.descr">Descr:</AppInput>
+				<AppInput v-model="post.img">Img:</AppInput>
+				<AppTextArea v-model="post.content">Content:</AppTextArea>
+
+				<div class="controls">
+					<AppButton class="btnDanger" @click="cansel">Cansel</AppButton>
+					<AppButton @click="onSubmit">Save</AppButton>
+				</div>
+			</form>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			post: {
+				title: '',
+				descr: '',
+				img: '',
+				content: ''
+			}
+		}
+	},
+	methods: {
+		cansel() {
+			this.$router.push('/admin')
+		},
+		onSubmit() {
+			this.$emit('submit', this.post)
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.controls {
+	margin-top: 40px;
+	text-align: center;
+}
+</style>
---
+++ nuxt-blog/layouts/admin.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="wrapper">
+		<AppHeader />
+		<div class="wrapper-content wrapper-content--fixed">
+			<Intro title="Admin page">
+				<nuxt-link to="/admin" class="link linkWhite">Admin</nuxt-link>
+				<nuxt-link to="/admin/new-post" class="link linkWhite"
+					>New Post</nuxt-link
+				>
+			</Intro>
+			<nuxt />
+		</div>
+	</div>
+</template>
+
+<script>
+import AppHeader from '@/components/layouts/AppHeader'
+export default {
+	components: {
+		AppHeader
+	},
+	layout: 'admin'
+}
+</script>
