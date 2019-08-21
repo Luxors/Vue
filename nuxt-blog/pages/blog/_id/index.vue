27121ac31ed40abcb8d45961ec37818953a3f077
Luxors <luxors@mail.com>
2019-08-21 19:27:43 +0300

    backend #8

--- nuxt-blog/pages/blog/_id/index.vue
+++ nuxt-blog/pages/blog/_id/index.vue
@@ -9,42 +9,56 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 import Post from '~/components/blog/Post'
 import NewComment from '~/components/comments/NewComment'
 import Comments from '~/components/comments/Comments'
 
 export default {
 	components: { Post, NewComment, Comments },
-	data() {
+	async asyncData(context) {
+		const [post, comments] = await Promise.all([
+			axios.get(
+				`https://luxors-blog-nuxt.firebaseio.com/posts/${context.params.id}.json`
+			),
+			axios.get(`https://luxors-blog-nuxt.firebaseio.com/comments.json`)
+		])
 		return {
-			post: {
-				id: 1,
-				title: '1 post',
-				descr: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. 1',
-				img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
-				content:
-					'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, nobis ipsa placeat ea consequuntur modi quos quas earum amet itaque, delectus nemo tempora necessitatibus similique eos illo in eius doloremque dolorem velit? Itaque eum sapiente repellat, minus voluptas perferendis hic repudiandae ex. Sed expedita possimus excepturi, iste autem veniam dolor obcaecati laboriosam alias voluptas deserunt fugiat molestiae aperiam est perferendis quia quae, repellendus aut totam labore recusandae quidem harum. Inventore non possimus hic! Doloribus accusamus, fugiat perspiciatis illum porro omnis dolore repellat architecto eius cupiditate cum animi labore suscipit! Sit repellat corporis reprehenderit voluptates. Nam unde necessitatibus consequuntur recusandae suscipit!'
-			},
-			comments: [
-				{
-					name: 'Alex',
-					text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-				},
-				{
-					name: 'Pol',
-					text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-				},
-				{
-					name: 'Don',
-					text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-				},
-				{
-					name: 'Bill',
-					text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-				}
-			]
+			post: post.data,
+			comments: comments.data
 		}
 	}
+	// data() {
+	// 	return {
+	// 		post: {
+	// 			id: 1,
+	// 			title: '1 post',
+	// 			descr: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. 1',
+	// 			img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
+	// 			content:
+	// 				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, nobis ipsa placeat ea consequuntur modi quos quas earum amet itaque, delectus nemo tempora necessitatibus similique eos illo in eius doloremque dolorem velit? Itaque eum sapiente repellat, minus voluptas perferendis hic repudiandae ex. Sed expedita possimus excepturi, iste autem veniam dolor obcaecati laboriosam alias voluptas deserunt fugiat molestiae aperiam est perferendis quia quae, repellendus aut totam labore recusandae quidem harum. Inventore non possimus hic! Doloribus accusamus, fugiat perspiciatis illum porro omnis dolore repellat architecto eius cupiditate cum animi labore suscipit! Sit repellat corporis reprehenderit voluptates. Nam unde necessitatibus consequuntur recusandae suscipit!'
+	// 		},
+	// 		comments: [
+	// 			{
+	// 				name: 'Alex',
+	// 				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+	// 			},
+	// 			{
+	// 				name: 'Pol',
+	// 				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+	// 			},
+	// 			{
+	// 				name: 'Don',
+	// 				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+	// 			},
+	// 			{
+	// 				name: 'Bill',
+	// 				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+	// 			}
+	// 		]
+	// 	}
+	// }
 }
 </script>
 
