9af15686b7b716de0c940e076ab55676b16ec026
Yaroslav V. Derkach <Luxors@mail.com>
2024-11-18 13:58:23 +0300

    add vue & components autoimport

---
+++ userCRM_v3/src/pages/Home.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+// import { ref } from 'vue';
+
+const users = ref([])
+</script>
+
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section class="container users">
+      <h1>Users page</h1>
+      <div class="tabel-container">
+        <table>
+          <!-- Head -->
+          <thead>
+            <tr>
+              <th @click="sort('firstName')">Name&#8595;</th>
+
+              <th @click="sort('lastName')">Last name&#8595;</th>
+
+              <th @click="sort('age')">Age&#8595;</th>
+
+              <th @click="sort('gender')">Gender&#8595;</th>
+            </tr>
+          </thead>
+
+          <!-- Body -->
+          <tbody>
+            <tr
+              v-for="user in usersSort"
+              :key="user.id"
+            >
+              <td>
+                <img
+                  :src="user.image"
+                  :alt="user.image"
+                >
+                <span>{{ user.firstName }}</span>
+              </td>
+              <td>{{ user.lastName }}</td>
+              <td> {{ user.age }} </td>
+              <td> {{ user.gender }} </td>
+            </tr>
+
+          </tbody>
+
+        </table>
+      </div>
+      <footer>
+        <p>Debug: sort: {{ currentSort }}, dir: {{ currentSortDir }} </p>
+        <p>page: {{ this.page.current }}, length: {{ this.page.length }} </p>
+      </footer>
+    </section>
+
+    <!-- Buttons -->
+    <footer class="container footer">
+      <div class="button-list">
+        <button
+          class="btn btnPrimary"
+          @click="prevPage"
+        >&#8592;</button>
+        <button
+          class="btn btnPrimary"
+          @click="nextPage"
+        >&#8594;</button>
+      </div>
+    </footer>
+  </div>
+
+</template>
+
+<style scoped></style>
--- userCRM_v3/vite.config.ts
+++ userCRM_v3/vite.config.ts
@@ -2,17 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      // imports: ['vue', 'vue-router', '@vueuse/core', '@vueuse/math'],
+      // dirs: ['./src/composables/**/*', './src/utils'],
+      imports: ['vue', 'vue-router'],
+      dts: './imports.d.ts',
+    }),
+    Components({
+      dts: true,
+    }),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
