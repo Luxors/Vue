b88d828e5b955c83ae519eab9f08bc9ad994739b
Yaroslav V. Derkach <Luxors@mail.com>
2024-11-18 11:25:13 +0300

    +

--- userCRM/src/App.vue
+++ userCRM/src/App.vue
@@ -1,53 +1,58 @@
 <template>
-	<div class="wrapper">
+  <div class="wrapper">
 
-		<header>
-			<nav class="navbar">
-				<div class="container">
-					<div class="navbar-content">
-						<div class="logo">User CRM</div>
-						<ul class="navbar-list">
-							<li class="navbar-item" v-for="link in links" :key="link.title">
-								<router-link class="navbar-link" :to="link.url">{{ link.title }}</router-link>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</header>
+    <header>
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-content">
+            <div class="logo">User CRM</div>
+            <ul class="navbar-list">
+              <li
+                class="navbar-item"
+                v-for="link in links"
+                :key="link.title"
+              >
+                <router-link
+                  class="navbar-link"
+                  :to="link.url"
+                >{{ link.title }}</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
 
-		<router-view></router-view>
+    <router-view></router-view>
 
-		<!-- <footer>Footer</footer> -->
+    <!-- <footer>Footer</footer> -->
 
-	</div>
+  </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			links: [
-				{
-					title: 'Home',
-					url: '/'
-				},
-				{
-					title: 'Users',
-					url: '/users'
-				}
-			]
-		}
-	}
+  data() {
+    return {
+      links: [
+        {
+          title: 'Home',
+          url: '/'
+        },
+        {
+          title: 'Users',
+          url: '/users'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .navbar-link {
-	&.router-link-exact-active {
-		color: #494ce8;
-		}
-	}
-
+  &.router-link-exact-active {
+    color: #494ce8;
+  }
+}
 </style>
-
